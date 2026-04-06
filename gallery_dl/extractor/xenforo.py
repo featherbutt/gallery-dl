featherbutt/gallery-dl

commit db297a9eac815309524046b0af1e09c330d52e3a

[xenforo] decode '/redirect/' URLs (#9364)

gallery_dl/extractor/xenforo.py
@@ -79,8 +79,11 @@ class XenforoExtractor(BaseExtractor):
                             params = text.parse_query(text.unescape(ext[24:]))
                             ext = binascii.a2b_base64(params["url"]).decode()
                         elif ext.startswith("/redirect/"):
-                            ext = text.unescape(text.extr(
-                                ext, ">", "<").strip())
+                            if to := text.extr(ext, "?to=", "&"):
+                                ext = binascii.a2b_base64(to).decode()
+                            else:
+                                ext = text.unescape(text.extr(
+                                    ext, ">", "<").strip())
                         else:
                             continue
                     elif '"' in ext:

test/results/simpcity.py
@@ -299,6 +299,20 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://simpcity.cr/threads/alison-brie.11057/post-26955359",
+    "#comment" : "base64-encoded '/redirect/' URLs (#9364)",
+    "#category": ("xenforo", "simpcity", "post"),
+    "#class"   : xenforo.XenforoPostExtractor,
+    "#auth"    : True,
+    "#range"   : "1-3",
+    "#results" : (
+        "https://pixhost.to/show/7595/627084349_img_7411.jpg",
+        "https://pixhost.to/show/7595/627084353_img_7412.jpg",
+        "https://pixhost.to/show/7595/627084355_img_7413.jpg",
+    ),
+},
+
+{
     "#url"     : "https://simpcity.cr/threads/alua-tatakai.89490/",
     "#category": ("xenforo", "simpcity", "thread"),
     "#class"   : xenforo.XenforoThreadExtractor,

commit ce875d4e0f38b51724f5c72c55857f1aadc7c858

[xenforo] fix 'binascii.Error: Incorrect padding'
fixes regression introduced in db297a9eac815309524046b0af1e09c330d52e3a

gallery_dl/extractor/xenforo.py
@@ -79,11 +79,10 @@ class XenforoExtractor(BaseExtractor):
                             params = text.parse_query(text.unescape(ext[24:]))
                             ext = binascii.a2b_base64(params["url"]).decode()
                         elif ext.startswith("/redirect/"):
-                            if to := text.extr(ext, "?to=", "&"):
-                                ext = binascii.a2b_base64(to).decode()
-                            else:
-                                ext = text.unescape(text.extr(
-                                    ext, ">", "<").strip())
+                            if html := text.extr(ext, ">", "<").strip():
+                                ext = text.unescape(html)
+                            elif to := text.extr(ext, "?to=", "&"):
+                                ext = binascii.a2b_base64(to + "==").decode()
                         else:
                             continue
                     elif '"' in ext: