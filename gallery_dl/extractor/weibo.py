featherbutt/gallery-dl

commit 156308247bff58be9996d2f2d84a2cd3aa8713d5

[weibo] add 'likes' option (gh#9366)
- provide 'status["like"]' metadata
- skip like / 赞过 posts by default

gallery_dl/extractor/weibo.py
@@ -37,6 +37,7 @@ class WeiboExtractor(Extractor):
         self.longtext = self.config("text", False)
         self.videos = self.config("videos", True)
         self.movies = self.config("movies", False)
+        self.likes = self.config("likes", False)
         self.gifs = self.config("gifs", True)
         self.gifs_video = (self.gifs == "video")
 
@@ -76,6 +77,7 @@ class WeiboExtractor(Extractor):
 
     def items(self):
         original_retweets = (self.retweets == "original")
+        is_like = text.re(r"\d-\d\d?赞过").search
 
         for status in self.statuses():
 
@@ -99,6 +101,14 @@ class WeiboExtractor(Extractor):
             else:
                 files = []
                 self._extract_status(status, files)
+
+            if is_like(status["title"]["text"]):
+                if not self.likes:
+                    self.log.debug("Skipping %s (赞过 like)", status["id"])
+                    continue
+                status["like"] = True
+            else:
+                status["like"] = False
 
             if self.longtext and status.get("isLongText") and \
                     status["text"].endswith('class="expand">展开</span>'):

docs/gallery-dl.conf
@@ -1031,6 +1031,7 @@
 
             "gifs"     : true,
             "include"  : ["feed"],
+            "likes"    : false,
             "livephoto": true,
             "movies"   : false,
             "retweets" : false,

commit 64d94d5a377ac80d7cdb5270700dce443cce5ba9

[weibo] fix "KeyError: 'title'" (#47)
fixes regression introduced in 156308247bff58be9996d2f2d84a2cd3aa8713d5

gallery_dl/extractor/weibo.py
@@ -102,13 +102,16 @@ class WeiboExtractor(Extractor):
                 files = []
                 self._extract_status(status, files)
 
-            if is_like(status["title"]["text"]):
-                if not self.likes:
-                    self.log.debug("Skipping %s (赞过 like)", status["id"])
-                    continue
-                status["like"] = True
+            if title := status.get("title"):
+                if is_like(title.get("text") or ""):
+                    if not self.likes:
+                        self.log.debug("Skipping %s (赞过 like)", status["id"])
+                        continue
+                    status["like"] = True
+                else:
+                    status["like"] = False
             else:
-                status["like"] = False
+                status["like"] = None
 
             if self.longtext and status.get("isLongText") and \
                     status["text"].endswith('class="expand">展开</span>'):