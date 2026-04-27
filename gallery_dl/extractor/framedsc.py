56af0ef91bdab653b02bb415ad0df23b4a89d4df
cnk2024 <chloenahlin@gmail.com>
2026-04-27 17:53:38 -0400

    fix: correct copyright

--- gallery_dl/extractor/framedsc.py
+++ gallery_dl/extractor/framedsc.py
@@ -1,10 +1,8 @@
-# # -*- coding: utf-8 -*-
-
-# # Copyright 2026 Mike Fährmann
-# #
-# # This program is free software; you can redistribute it and/or modify
-# # it under the terms of the GNU General Public License version 2 as
-# # published by the Free Software Foundation.
+# -*- coding: utf-8 -*-
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
 
 # """Extractors for https://framedsc.com/"""
 
